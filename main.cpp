30304d170a8b6b3d243383e911b6e32870f38846
Oliquino <425003510@ntc.edu.ph>
2026-09-09 18:20:55 +0800

    Initial C++ student information system

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+ // Display the student information system header and details
+ cout << "==============================" << endl;
+ cout << " STUDENT INFORMATION SYSTEM" << endl;
+ cout << "==============================" << endl;
+ cout << endl;
+ cout << "Name: Juan Dela Cruz" << endl;
+ cout << "Course: BSIT" << endl;
+ cout << "Year Level: 2" << endl;
+ cout << endl;
+ cout << "Welcome to the system!" << endl;
+ return 0;
+}
+
